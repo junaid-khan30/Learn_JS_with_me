c4553074723119f726985bbf1b3dec5ba4380bb0
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-04-27 22:42:25 +0530

    Data types

---
+++ Introduction/datatypes.js
@@ -0,0 +1,30 @@
+// Data Types
+// var, let, const
+
+
+
+/*1 : Any varible it can be declared with var let or const whether it maybe float , int , string and 
+we don't have to mentions any range it internally built in , Programmers need not to specify the data type, ranges and format specifier like C language
+
+2 : JS is loosely typed language we dont have to recognize the data type
+
+3 : In C its curly braces and in js its square brackets 
+
+4 : to.fixed is to control the matissa value you want 
+
+*/ 
+console.log("Hello World!");
+
+var num = 45;
+
+var fname = "Prash";
+
+var lname = "B";
+
+var arr = [1, 5, 3, 8, 32];
+
+var salary = 4500.46;
+console.log(arr);
+console.log(0.1 + 0.2);
+console.log((0.1 + 0.2).toFixed(52));
+
--- Introduction/hello.js
+++ Introduction/hello.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+// Data Types
+// var, let, const
+
+
+
+/*1 : Any varible it can be declared with var let or const whether it maybe float , int , string and 
+we don't have to mentions any range it internally built in , Programmers need not to specify the data type, ranges and format specifier like C language
+
+
+*/ 
