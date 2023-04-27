f6ddc4f33b522e39988136839230c23b72a7ac2b
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-04-27 23:39:28 +0530

    length of variable

---
+++ Introduction/4.js
@@ -0,0 +1,10 @@
+var fname = 'Junaid';
+var lname = "Khan";
+var city = 'Hyderabad';
+
+//String Properties 
+
+console.log(fname.length);
+console.log(fname[0]);
+console.log(fname[1]);
+console.log(fname[2]);
