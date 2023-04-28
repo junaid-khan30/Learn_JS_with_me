0a4f73ca2a56e96f46e41be6d457866b830fc88c
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-04-28 22:04:27 +0530

    .split methods

---
+++ loops/4.js
@@ -0,0 +1,17 @@
+/*// .split converts strings into array 
+//.split with split("") splits every thing character by character in one array
+//.split with single space with make every word seprated 
+//we rarely use 5 line code  we use 6 and 7 frequently
+*/
+
+var str = "Hello Good Evening Freinds";
+console.log(str.split());
+console.log(str.split(''));
+console.log(str.split(' '));
+
+//Word Count 
+console.log(str.split(" ").length);
+//Characte Count 
+console.log(str.split("").length);
+
+
