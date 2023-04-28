fa975c6a283050fe00f04e83b17aadd3a12fb9ac
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-04-28 21:11:49 +0530

    pattern

---
+++ loops/2.js
@@ -0,0 +1,23 @@
+/*
+Print the Below Pattern 
+H
+H E 
+H E L
+H E L L
+H E L L O
+*/
+
+var str = "HELLO";
+var temp = "";
+//console.log(str[0]);
+//console.log(`${str[0]}${str[1]}`);
+//console.log(`${str[0]}${str[1]}${str[2]}`);
+
+
+
+
+for (var i = 0; i < str.length; i++) {
+    console.log(`${temp}${str[i]}`);
+    temp += `${str[i]} `;
+
+}
