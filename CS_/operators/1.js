5ca8e86a9cdcfd233ece02997dbc1536fa2f5eb5
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-03 20:51:59 +0530

    Assignment Operator

---
+++ CS_/operators/1.js
@@ -0,0 +1,14 @@
+//Assignment Operators
+
+var a = 2 + 5;
+a = 2 % 5;
+console.log(a);
+var x = 5, y = 3;
+var result = 1;
+for (var i=1; i<=y; i++){
+    result *= x;
+}
+console.log(`using loops : ${result}`);
+console.log(`Using ** : `, x ** y);
+
+
