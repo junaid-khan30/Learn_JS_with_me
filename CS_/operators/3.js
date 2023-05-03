7b77c04743412fc6d49928492575639e0f16dbb9
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-03 22:35:38 +0530

    Operators

---
+++ CS_/operators/3.js
@@ -0,0 +1,27 @@
+// leftshift operator  
+console.log(5<<2);
+console.log(5<<-2);
+// rightshift operator
+console.log(5>>2);
+console.log(5>>-2);
+console.log(-5>>2);
+console.log(-5>>>2);
+
+//unsigned right shift operator 
+console.log(`-5>>>2`);
+
+//bitwise AND operator 
+console.log(`bitwise AND : ${7 & 5}`);
+console.log(`bitwise AND : ${-6 & -2}`);
+
+//bitwise OR operator 
+console.log(`bitwise OR : ${7 | 5}`);
+console.log(`bitwise OR : ${-6 | -2 }`);
+
+//Logical AND Operator (&&)
+console.log(`Logical AND : ${7 && 5}`);
+console.log(`Logical AND : ${-6 && -3}`);
+
+//Logical OR Operator (||)
+console.log(`Logical OR : ${9 || 8 || 10}`);
+console.log(`Logical OR : ${8 || 7 || 9}`);
