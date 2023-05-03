3eef5803cce1da96b4aeef962791680dfac2e201
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-03 21:05:07 +0530

    finding squareroot with function

---
+++ CS_/operators/2.js
@@ -0,0 +1,13 @@
+//Find Square roots using ** operators
+console.log(625**0.5);
+console.log(2**0.5);
+
+//In JS, We have squareroot function for this : 
+function squareroot(num){
+    return num ** 0.5;
+} 
+console.log(squareroot(100));
+
+var result = squareroot(125);
+console.log(result);
+
