c74425e50887c94dd3360a263ad9eb079f015966
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-05 20:34:52 +0530

    Accepting Armstrong Numbers

---
+++ CS_/cli-app/2.js
@@ -0,0 +1,18 @@
+var readlineSync = require('readline-sync');
+
+var inputNum = readlineSync.question("Enter the value of N : "); //input N
+
+//Solution -1 : Strings Method
+function isArmstrong(num){
+    var digits = num.split('');
+    var sum = 0;
+    for(var i=0; i<digits.length; i++){
+        sum += digits[i] ** digits.length;
+    }
+    if(sum == num)return true;
+    else return false;
+    }
+
+    var result = isArmstrong(inputNum);
+
+    console.log(result);
