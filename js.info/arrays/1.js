999e7a2e800141f0431b0a3af758e47f995d60f9
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-09 13:13:07 +0530

    array.at

---
+++ js.info/arrays/1.js
@@ -0,0 +1,7 @@
+//Array.at
+var array1 = [5, 12, 6, 412, 44];
+var index = 2;
+console.log(` ${index}  is ${array1.at(index)}`);//"Expected output is "Using an index of 2 the item returned is 6"
+ index = -2;
+ console.log(` ${index}  is ${array1.at(index)}`);
+ //Expected output is "Using an index of -2 itme returned is 412"
