7e3b409a6ed828ef9358af0f61210e566b260b76
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-10 20:39:59 +0530

    extracting files

---
+++ CS_/objects/10.js
@@ -0,0 +1,13 @@
+// using data of data.js file here 
+var data = require("./data.js");
+
+function extractCountry(data, countryName){
+    var countries = [];
+    for ( var i =0; i < data.length; i++){
+        if (data[i].country.includes(countryName)){
+            countries.push(data[i]);
+        }
+    }
+    return countries;
+}
+console.log(extractCountry(data,"Egypt"));
