358a640007d13e488c58f6c3ef1d6f212192d1da
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-13 23:29:33 +0530

    async

---
+++ CS_/async/4.js
@@ -0,0 +1,13 @@
+console.log("Hello World No 22");
+setTimeout(function () {
+    console.log("Hello Hell");
+}, 15000);
+xyz();
+console.log("Hello World 2");
+abc();
+function abc() {
+    console.log("ABC");
+}
+function xyz() {
+    console.log("XYZ");
+}
