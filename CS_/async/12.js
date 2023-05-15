151708e9bf3ee7105ac5c8d27580de4786df0be6
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-15 18:10:31 +0530

    Set Timer

---
+++ CS_/async/12.js
@@ -0,0 +1,12 @@
+etTimeout(function () {
+    console.log("I am Just Getting Started with Adv. JS");
+}, 2000);
+console.log("The Start");
+function abc() {
+    console.log("Hello There");
+    setTimeout(function () {
+        console.log("Hello World");
+    }, 3000);
+}
+abc();
+console.log("The End");
