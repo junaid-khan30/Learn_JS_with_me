c120a4cc1d4d9cd2e7ad654cc495dc9901b7703a
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-15 18:09:01 +0530

    Set Timer

---
+++ CS_/async/10.js
@@ -0,0 +1,12 @@
+//Fat Arrow function method
+// function doSum(a, b) {
+//     return a + b;
+// }
+
+// var doSum = (a, b) => {
+//     return a + b;
+// }
+
+var doSum = (a, b) => a + b;
+
+console.log(doSum(55, 55));
