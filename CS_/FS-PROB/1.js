94ffc1849f1bc16c1b84b400deaabad083f57a5f
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-16 23:37:10 +0530

    FS API

--- CS_/FS-PROB/1.js
+++ CS_/FS-PROB/1.js
@@ -1,12 +1,20 @@
-//Write a Program to Read the FIle Data, Convert the File Data into UPPERCASE
-//and create the new File with the new buffer, Delete the original file after creating new file.
+// API - Application Programming Interface
 
 var fs = require("fs");
 
-fs.readFile("data.txt", (err,data) => {
+console.log("I am getting started with Adv. JS");
+
+
+fs.readFile("test.txt", (error, something) => {
     if (err) {
         console.log(err);
-    }else {
-        var newData
+    } else {
+        console.log(data.toString());
     }
-})
+});
+
+
+console.log("I am dying at the end of this class");
+
+// What is CB in the above code snippet?
+// Note : The CB is asynchronous
