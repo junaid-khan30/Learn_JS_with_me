0ab332fda56803e01f691848794d4729164dd76a
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-17 22:40:26 +0530

    sorting

--- CS_/cli-game/sort.js
+++ CS_/cli-game/sort.js
@@ -1,4 +1,4 @@
-ar scores = [99, 101, 1, 0, -123, 90, 1];
+var scores = [99, 101, 1, 0, -123, 90, 1];
 
 var cohort = ["prash", "abhinav", "sam", "sam", "adnan", "ritvik", "sania", "yaseera", "hanzala", "junaid"];
 console.log(scores.sort(function (a, b) { return b - a })); //Sort takes UTF(ASCII) as refernce
