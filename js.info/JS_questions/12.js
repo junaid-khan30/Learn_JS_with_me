22f21c6047c78126c0cca10cf8f571dc231a133a
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-19 20:12:41 +0530

    JS Questions

---
+++ js.info/JS_questions/12.js
@@ -0,0 +1,28 @@
+/*Analyze the following code block and write the output along with 4-5 lines of reasoning.*/
+
+var a = "Walter";
+var b = "Prash";
+console.log(a + b);
+console.log('"WalterPrash"');
+console.log("\"WalterPrash\"");
+console.log(`"${a}${b}"`);
+console.log(`"${a}\t\n${b}"`);
+/* console.log(a + b);
+Output: "WalterPrash"
+Reasoning: The + operator is used to concatenate the values of a and b. Therefore, "Walter" and "Prash" are combined to form "WalterPrash", which is the output.
+
+console.log('"WalterPrash"');
+Output: "WalterPrash"
+Reasoning: The console.log() function is used to output the string literal "WalterPrash", which is enclosed in double quotes. The output will be the same string.
+
+console.log("\"WalterPrash\"");
+Output: "WalterPrash"
+Reasoning: Here, the string literal "WalterPrash" is enclosed in double quotes and escaped using the backslash (\) to include the double quotes within the string. The output will be the same string.
+
+console.log("${a}${b}");
+Output: "WalterPrash"
+Reasoning: This line uses string interpolation with the template literal syntax (${}) to concatenate the values of a and b within double quotes. The output will be the same string, "WalterPrash".
+
+console.log("${a}\t\n${b}");
+Output: "Walter Prash"
+Reasoning: This line uses string interpolation with the template literal syntax to concatenate the values of a and b within double quotes. The special characters \t and \n are used to represent a tab and a new line, respectively. The output will be "Walter", followed by a tab character, a new line, and then "Prash".*/
