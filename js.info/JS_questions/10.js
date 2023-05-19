568b3f505fde773c06ef110f1b878f252c322e32
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-19 19:58:21 +0530

    JS Questions

---
+++ js.info/JS_questions/10.js
@@ -0,0 +1,23 @@
+/*Analyze the following code block and write the output along with 4-5 lines of reasoning.*/
+var t = 11;
+var j = 11.45;
+var p = -111.6567;
+var q = 22 / 7;
+console.log(typeof t, typeof j, typeof p, typeof q);
+/* var t = 11;
+t is assigned the value 11, which is an integer.
+
+var j = 11.45;
+j is assigned the value 11.45, which is a floating-point number or a decimal number.
+
+var p = -111.6567;
+p is assigned the value -111.6567, which is also a floating-point number.
+
+var q = 22 / 7;
+q is assigned the value of the result of 22 divided by 7, which is approximately 3.142857142857143. Again, this is a floating-point number.
+
+console.log(typeof t, typeof j, typeof p, typeof q);
+This line prints the type of each variable using the typeof operator.
+
+Output: "number number number number"
+Reasoning: The typeof operator returns the type of the operand as a string. Since all variables (t, j, p, and q) are assigned numeric values, their types are all "number". */
