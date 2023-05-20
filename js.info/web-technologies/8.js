4ed75c73091b23b5a164a6a9541f31a1c8316787
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-20 16:18:15 +0530

    web-tech

---
+++ js.info/web-technologies/8.js
@@ -0,0 +1,15 @@
+/*Analyze the following code block and write the output along with 4-5 lines of reasoning.*/
+var fname = "prash";
+var age = 99;
+var temp = "trash";
+var user = {
+    fname, age, temp
+}
+console.log(user);
+console.log(user.temp);
+console.log(); 
+/* he code block initializes three variables: fname with the value "prash", age with the value 99, and temp with the value "trash".
+The user object is created using object literal syntax, and its properties fname, age, and temp are assigned the corresponding values from the variables with the same names.
+The console.log(user) statement outputs the entire user object, which consists of the properties fname, age, and temp along with their respective values.
+The console.log(user.temp) statement accesses the temp property of the user object and outputs its value, which is "trash".
+The console.log() statement with no arguments will output an empty line.*/
