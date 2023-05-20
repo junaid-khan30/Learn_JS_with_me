ca62efa0e68dcaa05d7e5cf21938ebc5084e1329
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-20 16:33:51 +0530

    web-tech

---
+++ js.info/web-technologies/12.js
@@ -0,0 +1,10 @@
+/*Analyze the following code block and write the output along with 4-5 lines of reasoning.*/
+var num = 2;
+for (var i = 1; i <= 15; i++) {
+    console.log(`2 x ${i} = ${2 * i}`);
+}
+/* The code block initializes the variable num with the value 2.
+The for loop is executed 15 times, starting from i = 1 and incrementing i by 1 in each iteration until i reaches 15.
+Within the loop, the console.log statement prints a formatted string that displays the multiplication table for num.
+The string template uses the value of i to calculate 2 * i and displays it as part of the output.
+As a result, the loop generates and prints the multiplication table for 2 from 2 x 1 to 2 x 15, displaying the multiplication results for each iteration.*/
