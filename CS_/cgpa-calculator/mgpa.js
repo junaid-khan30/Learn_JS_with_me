bc28815bfb937bef6c4dec6a2ef7b1975de7e8eb
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-22 23:29:30 +0530

    cgpa-calculator

--- CS_/cgpa-calculator/mgpa.js
+++ CS_/cgpa-calculator/mgpa.js
@@ -1,6 +1,21 @@
-function mgpaCalculator(){
-    console.clear();
-    console.log("I am MGPA Calci");
+var readlineSync = require('readline-sync');
+
+function mgpaCalculator() {
+    var studentName = readlineSync.question("Enter Student Name : ");
+    var matNo = readlineSync.questionInt("Enter MAT Number : ");
+    var courseCount = readlineSync.questionInt(`Enter Number of Courses in MAT-${matNo} : `);
+    var courseCodes = [];
+    var courseName = [];
+    var courseCredits = [];
+    var gradePoints = [];
+
+    for (var i = 0; i < courseCount; i++) {
+        courseCodes[i] = readlineSync.question(`Enter Course Code ${i + 1}:`);
+        courseName[i] = readlineSync.question(`Enter Course Name for ${courseCodes[i]}`);
+        courseCredits[i] = readlineSync.question(`Enter Course Credits for ${courseName[i]}`);
+        gradePoints[i] = readlineSync.question(`Enter Obtained Grade Points for ${courseName[i]} :`);
+    }
+
 }
 
 module.exports = mgpaCalculator;
