4b89420316670da40f041a10fad881bcccad841d
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-23 22:29:45 +0530

    cgpa-calculator

--- CS_/cgpa-calculator/index.js
+++ CS_/cgpa-calculator/index.js
@@ -2,34 +2,35 @@ var readlineSync = require('readline-sync');
 var mgpaCalculator = require("./mgpa.js");
 var cgpaCalculator = require("./cgpa.js");
 
-while (true){
-console.log("-------------------");
-console.log("OPTIONS");
-console.log("-------------------");
-console.log("0 Exit Program");
-console.log("1 Calculate MGPA");
-console.log("2 Calculate CGPA");
-console.log("-------------------");
+while (true) {
+    console.clear();
+    console.log("---------------");
+    console.log("OPTIONS");
+    console.log("---------------");
+    console.log("0 Exit Program");
+    console.log("1 Calculate MGPA");
+    console.log("2 Calculate CGPA");
+    console.log("---------------");
 
-var userChoice = readlineSync.questionInt("Select from the Options Menu:");
-switch (userChoice) {
-    case 1:
-        mgpaCalculator();
-        break;
-    case 2:
-        cgpaCalculator();
-        break;
-    case 0:
-        console.clear();
+    var userChoice = readlineSync.questionInt("Select from the Options Menu : ");
+    switch (userChoice) {
+        case 1:
+            mgpaCalculator();
+            break;
+        case 2:
+            cgpaCalculator();
+            break;
+        case 0:
+            console.clear();
+            console.log("Okay! Exiting from program. Bye Bye...");
+            process.exit(0);
+            break;
+        default:
+            console.log("Invalid Option! Please Select Valiid Option From the Menu.");
+    }
+    var decisionToContinue = readlineSync.question("Want to perform again? y/N:");
+    if (decisionToContinue == 'n' || decisionToContinue == 'N') {
         console.log("Okay! Exiting from program. Bye Bye...");
         process.exit(0);
-        break;
-    default:
-        console.log("Invalid Option! Please Select Valid Option From the Menu.");
-}
-var decisionToContinue = readlineSync.question("Want to perform again? y/N:");
-if (decisionToContinue == 'n' || decisionToContinue == 'N'){
-    console.log("Okay! Exiting from program. Bye Bye...");
-    process.exit(0);
-}
+    }
 }
