a4a7a945b25146a396ec722e034f50a43cc01f6e
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-24 15:41:48 +0530

    random Numbers

---
+++ js.info/GAME-PROJECT/index.js
@@ -0,0 +1,21 @@
+var readlineSync = require('readline-sync');
+
+/*
+   (0,9) => Numbers between 0 and 9 but not 0,9 Exclusive
+   [0,9] => Numbers between 0-9 and 0,9 are inclusive 
+   [0,9) => 0 is inclusive and 9 is exclusive 
+
+    Interger (z) - Zahlen 
+
+*/
+
+function playGame() {
+    console.log(Math.random());//0-1(exclusive)
+    console.log(Math.random() * 10);// 0-9(including float numbes)
+    console.log(Math.floor(Math.random()* 10)); //0-9(without float value )
+
+    console.log(Math.floor(Math.random() * 10) + 1); //[1,10]
+
+    console.log(Math.floor(Math.random() * 10) + 5); //[5, 14]
+}
+playGame();
