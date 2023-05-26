deb4fece1783b2b3771f5ae044722882fb7e93e0
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-26 23:05:28 +0530

    pinging API and creating new json file

--- CS_/callbacks/3.js
+++ CS_/callbacks/3.js
@@ -1,9 +1,20 @@
 var axios = require("axios");
+var fs = require("fs/promises");
+
+
 
 axios.get("https://jsonplaceholder.typicode.com/users")
-.then((res)=>{
-     console.log(res.data);
-})
-.catch((err)=> {
-    console.error(err);
-})
+    .then((res) => {
+        var users = res.data;
+        fs.writeFile("users.json", JSON.stringify(users))
+            .then(() => {
+                console.log("File user.json is created");
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
+    })
+    .catch((err) => {
+        console.error("Network issue. Connection Error");
+    })
