0595b9ef829f674cab3cbb3cced33ed9ce39c44b
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-26 22:02:32 +0530

    callbacks

--- CS_/callbacks/2.js
+++ CS_/callbacks/2.js
@@ -1,10 +1,17 @@
 var fs = require("fs/promises");
 
+//Promise will go to 'then' block if it is a success
+//Promise will go to 'catch' block if it is a failure
+
+console.log("Hi I am First");
+
 fs.readFile("todos.json")
 .then((data)=>{
-    console.log(data);
+    var todos = JSON.parse(data);
+    console.log(todos[0]);
 })
 .catch((error)=>{
     console.error(err);
 })
 
+console.log("Hi I am Last");
