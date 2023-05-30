78bd864d01695ff64c81f931016c7b4f846e4ce8
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-30 21:22:39 +0530

    Firing API if its not PRIME

---
+++ CS_/PROMISES/6.js
@@ -0,0 +1,14 @@
+/*
+
+Step 1 : Generate Random number between 1-100 into num1 variable
+Step 2 : Generate Random number bertween 1-100 into num2 variable
+
+Step 3 : Fire the below API using axios 
+
+http://36.255.86.216/${num1}/${num2}
+
+Step 4 : If the sum in the res.data is Prime, Do not fire back the API. Stop The Program 
+
+Step 5 : Continue Firing the API, iF the res.data is not Prime 
+
+*/
