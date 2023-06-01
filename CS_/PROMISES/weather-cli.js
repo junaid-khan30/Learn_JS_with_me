5addd34eabec7f57c6d416575ca4db8b54f64170
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-01 22:47:31 +0530

    weather-cli

--- CS_/PROMISES/weather-cli.js
+++ CS_/PROMISES/weather-cli.js
@@ -1,8 +1,10 @@
 var readlineSync = require("readline-sync");
 var axios = require("axios");
 var clc = require("cli-color");
+var loading = require('loading-cli');
 
 var cityName = readlineSync.question(clc.yellow("Enter your City Name : "));
+var load = loading("Have some snacks, it wil take time for us ....").start();
 var api_key = "b3031bf47673eb8a19ee7c39450b3306";
 function getWeather(cityName) {
     return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_key}`);
@@ -11,7 +13,10 @@ function getWeather(cityName) {
 getWeather(cityName)
     .then((res) => {
         var temp = ((res.data.main.temp) - 273.15).toFixed(2);
-        console.log((clc.red(`${cityName} Current Weatehr is : ${temp} \u00B0C`)));
+        setTimeout(()=> {
+            console.log((clc.red(`${cityName} Current Weatehr is : ${temp} \u00B0C`)));
+            load.stop();
+        },4500);
     })
     .catch((err) => {
         console.log(err);
