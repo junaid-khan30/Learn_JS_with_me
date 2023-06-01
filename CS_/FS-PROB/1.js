e75f8aab852ed2f42cd3672509c829f617452b88
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-01 21:35:26 +0530

    fs-api

--- CS_/FS-PROB/1.js
+++ CS_/FS-PROB/1.js
@@ -1,11 +1,11 @@
 // API - Application Programming Interface
 
 var fs = require("fs");
-
+console.log(fs);
 console.log("I am getting started with Adv. JS");
 
 
-fs.readFile("test.txt", (error, something) => {
+fs.readFile("text.txt", (err, data) => {
     if (err) {
         console.log(err);
     } else {
@@ -16,5 +16,7 @@ fs.readFile("test.txt", (error, something) => {
 
 console.log("I am dying at the end of this class");
 
+
 // What is CB in the above code snippet?
 // Note : The CB is asynchronous
+// Call backs are asynchronous 
