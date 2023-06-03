abc1a84fc29369221995a9b7b34623c43a071dc1
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-03 21:29:34 +0530

    accesing Country codes from json data

--- CS_/FS-PROB/country-codes/file.js
+++ CS_/FS-PROB/country-codes/file.js
@@ -1,36 +1,16 @@
-var fs = require('fs');
-fs.readFile('cities.json', (err, data) => {
+var fs = require("fs");
+fs.readFile("cities.json", (err, data) => {
     if (err) {
-        console.error(err);
+        console.log(err);
     } else {
-        var countryCodes = [];
+        var CountryCodes = [];
         var list = JSON.parse(data);
         for (var i = 0; i < list.length; i++) {
-            if (!countryCodes.includes(list[i].country)) {
-                countryCodes.push(list[i].country);
+            if(!CountryCodes.includes(list[i].country)){
+                CountryCodes.push(list[i].country);
             }
         }
-        for (var i = 0; i < countryCodes.length; i++) {
-            var cities = countryData(countryCodes[i], list);
-            fs.writeFile(`${countryCodes[i]}.json`, JSON.stringify(cities), (err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(`file has been created.`);
-                }
-            })
-        }
-
+        console.log(CountryCodes); 
+        console.log(CountryCodes.length);
     }
 });
-
-
-function countryData(country, list) {
-    var cities = [];
-    for (var i = 0; i < list.length; i++) {
-        if (list[i].country === country) {
-            cities.push(list[i]);
-        }
-    }
-    return cities;
-}
