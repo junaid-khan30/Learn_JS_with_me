e2c775b7c13d7166633bc4563eab799c90df3c48
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-03 22:49:31 +0530

    country codes

--- js.info/FS-PROBS/4.js
+++ js.info/FS-PROBS/4.js
@@ -10,18 +10,25 @@ fs.readFile("countries.json",(err,data)=>{
                 countryCodes.push(list[i].country);
             }
         }
-        console.log(countryCodes);
-        console.log(countryCodes.length);
-        console.log(list.length);
+        for(var i=0; i<countryCodes.length;i++){
+             var cities = countryData(countryCodes[i],list);
+             fs.writeFile(`${countryCodes[i]}.json`,JSON.stringify(cities),(err)=>{
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log(` file has been created.`);
+                }
+             })
+        }
      }
 });
-/*
+
 function countryData(country, list){
     var cities = [];
     for (var i=0; i < list.length; i++){
         if(list[i].country === country){
-            countries.push(list[i]);
+            cities.push(list[i]);
         }
     }
-    return countries;
-}*/
+    return cities;
+}
