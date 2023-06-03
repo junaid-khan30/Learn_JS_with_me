f2289a67dd604fcbfa8a28b1de68bf05dba073fc
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-04 02:41:01 +0530

    fs-api

--- CS_/FS-PROB/country-codes/file.js
+++ CS_/FS-PROB/country-codes/file.js
@@ -1,16 +1,34 @@
 var fs = require("fs");
-fs.readFile("cities.json", (err, data) => {
-    if (err) {
+fs.readFile("countries.json",(err,data)=>{
+    if(err){
         console.log(err);
-    } else {
-        var CountryCodes = [];
+    }else{
+        var countryCodes = [];
         var list = JSON.parse(data);
-        for (var i = 0; i < list.length; i++) {
-            if(!CountryCodes.includes(list[i].country)){
-                CountryCodes.push(list[i].country);
+        for(var i =0; i<list.length; i++){
+            if(!countryCodes.includes(list[i].country)){
+                countryCodes.push(list[i].country);
             }
         }
-        console.log(CountryCodes); 
-        console.log(CountryCodes.length);
-    }
+        for(var i=0; i<countryCodes.length;i++){
+             var cities = countryData(countryCodes[i],list);
+             fs.writeFile(`${countryCodes[i]}.json`,JSON.stringify(cities),(err)=>{
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log(` file has been created.`);
+                }
+             })
+        }
+     }
 });
+
+function countryData(country, list){
+    var cities = [];
+    for (var i=0; i < list.length; i++){
+        if(list[i].country === country){
+            cities.push(list[i]);
+        }
+    }
+    return cities;
+}
