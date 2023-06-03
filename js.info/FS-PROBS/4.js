312367e9d7a72b448395af7db9dc7d0b70aca979
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-03 21:45:55 +0530

    country codes

---
+++ js.info/FS-PROBS/4.js
@@ -0,0 +1,17 @@
+var fs = require("fs");
+fs.readFile("countries.json",(err,data)=>{
+    if(err){
+        console.log(err);
+    }else{
+        var countryCodes = [];
+        var list = JSON.parse(data);
+        for(var i =0; i<list.length; i++){
+            if(!countryCodes.includes(list[i].country)){
+                countryCodes.push(list[i].country);
+            }
+        }
+        console.log(countryCodes);
+        console.log(countryCodes.length);
+
+    }
+});
