21e81196e6a2ca642ea49b5da4266a2d58c63c00
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-04 21:04:56 +0530

    callbacks

--- CS_/callbacks/3.js
+++ CS_/callbacks/3.js
@@ -1,10 +1,12 @@
 var axios = require("axios");
 
-axios.get("http://jsonplaceholder.typicode.com/users")
+axios.get("http://jsondaughter.typicode.com/users")
      .then((res)=> {
         console.log(res.data);
      })
      .catch((err)=>{
-        console.error(err);
+        console.error("Hey Dumb! type your url properly");
      })
 //the api are getting from another server by axios.get
+//axios is a http client, and its a promise based library which helps to make http network calls 
+//axios is like curl in linux 
