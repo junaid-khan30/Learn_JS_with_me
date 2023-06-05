d6a73db9b63cfe0ce774126da858e924615e887d
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-05 21:49:05 +0530

    async await

--- CS_/PROMISES/awaits/1.js
+++ CS_/PROMISES/awaits/1.js
@@ -4,4 +4,16 @@ var fs = require("fs/promises");
 NOTE 1 : Async await are not replacement to Promises 
 NOTE 2 : Async await is an extension to consume an existing promise
 
+Step 1 : Entire async code must be wrapped in a function with a try catch block
+
 */
+
+async function main(){
+    try{
+        var data = await fs.readFile("todos.json");
+        console.log(JSON.parse(data));
+    }catch (error){
+        console.error(err);
+    }
+}
+main();
