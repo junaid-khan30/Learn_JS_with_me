eeebd26f0a6bb72526bbe872b5de85fae2ed4631
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-06 22:07:44 +0530

    base64

--- CS_/base64/2.js
+++ CS_/base64/2.js
@@ -3,7 +3,7 @@ function base64Encoding (inputStr){
     //Step1 : Split the word into ASCII Characters
     console.log(inputStr);
     var asciiChars = [];
-    for(var i=0; i<inputStr[i].length; i++){
+    for(var i=0; i<inputStr.length; i++){
         asciiChars.push(inputStr[i].charCodeAt(0));
     }
     console.log(asciiChars);
