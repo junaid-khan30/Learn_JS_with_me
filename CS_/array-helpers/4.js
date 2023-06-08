5c09c0a4a0e83d62350caaa327534e5fa68f070f
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-08 21:50:07 +0530

    Array-Helper

--- CS_/array-helpers/4.js
+++ CS_/array-helpers/4.js
@@ -1,10 +1,18 @@
 var arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-arr.map((ele)=>{
+ var result = arr.map((ele)=>{
     console.log(ele);
+    return "Donkey"
 });
 
 console.log("------------");
-arr.forEach((ele)=>{
+var result2 = arr.forEach((ele)=>{
     console.log(ele);
-})
+    return "Donkey"
+});
+
+console.log(result);
+console.log(result2);
+
+//Array.map() return ele
+//Array.forEach() doesn't return ele
