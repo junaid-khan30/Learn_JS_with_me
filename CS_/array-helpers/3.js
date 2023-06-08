e8bb7991d1d5bd4da8a6e1da5744825893af07cd
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-08 21:45:33 +0530

    Array-Helper

--- CS_/array-helpers/3.js
+++ CS_/array-helpers/3.js
@@ -1,6 +1,6 @@
 var arr = [100, 99, 10, 1000, 88, 99, 99];
 var result = [];
 
-arr.forEach(function (ele)=>{
-    result.push()
+arr.forEach( (ele)=>{
+    result.push(arr[i]);
 })
--- CS_/array-helpers/4.js
+++ CS_/array-helpers/4.js
@@ -3,3 +3,8 @@ var arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 arr.map((ele)=>{
     console.log(ele);
 });
+
+console.log("------------");
+arr.forEach((ele)=>{
+    console.log(ele);
+})
