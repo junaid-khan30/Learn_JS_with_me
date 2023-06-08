f11af58badfde3cb917627ac9f82d519776f91f7
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-08 21:40:09 +0530

    Array-Helper

---
+++ CS_/array-helpers/2.js
@@ -0,0 +1,12 @@
+//Array Helper Methods
+//forEach()
+
+var arr = [100, 99, 10, 1000, 88, 99, 99];
+var result = [];
+for(var i=0; i<arr.length; i++){
+    result = (arr[i] * 2);
+    console.log(result);
+}
+arr.forEach(function (ele, index){
+    console.log(`${ele} is located at ${index} index in arr Array`);
+});
