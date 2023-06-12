4d9b25fbbaa2cb14073eecd2079e0b49cf14d6b6
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-12 22:11:24 +0530

    github profile picture

--- CS_/array-helpers/6.js
+++ CS_/array-helpers/6.js
@@ -6,8 +6,10 @@ async function main(){
         var username = readlineSync.question("Enter your github username : ");
         var res = await axios.get(`http://api.github.com/users/${username}`);
         var profile_pic_url = res.data.avatar_url;
-        var res2 = await axios.get(profile_pic_url);
-        console.log(res2.data);
+        var res2 = await axios.get(profile_pic_url,{
+            responseType: 'arraybuffer'
+        });
+        await fs.writeFile(`${username}.png`, res2.data);
     }catch (error){
         console.log("Invalid Github Username/Server issue");
     }
