5d6304097b0aba842135f7a7912bdbc6b877a8bb
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-13 20:47:57 +0530

    array-helper

--- CS_/array-helpers/6.js
+++ CS_/array-helpers/6.js
@@ -1,17 +1,23 @@
-var axios = require("axios");
-var readlineSync = require("readline-sync");
-
-async function main(){
+import axios from "axios";
+// var axios = require("axios");
+import readlineSync from "readline-sync";
+// var readlineSync = require("readline-sync");
+import fs from "fs/promises";
+// var fs = require("fs/promises");
+import terminalImage from "terminal-image";
+async function main() {
     try {
         var username = readlineSync.question("Enter your github username : ");
-        var res = await axios.get(`http://api.github.com/users/${username}`);
+        var res = await axios.get(`https://api.github.com/users/${username}`);
         var profile_pic_url = res.data.avatar_url;
-        var res2 = await axios.get(profile_pic_url,{
+        var res2 = await axios.get(profile_pic_url, {
             responseType: 'arraybuffer'
         });
         await fs.writeFile(`${username}.png`, res2.data);
-    }catch (error){
-        console.log("Invalid Github Username/Server issue");
+        var imageBuffer = await terminalImage.file(`${username}.png`, {width : '50%', height : "50%"});
+        console.log(imageBuffer);
+    } catch (error) {
+        console.log("Invalid GitHub Username/Server issue");
     }
 }
 main();
