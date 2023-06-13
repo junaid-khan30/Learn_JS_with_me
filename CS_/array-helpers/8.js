5107fc39cb9a919ac743d0b712cb748d254fed49
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-13 20:49:07 +0530

    array-helper

---
+++ CS_/array-helpers/8.js
@@ -0,0 +1,9 @@
+// var data = require("./7.js");
+import data from "./7.js";
+
+console.log(data);
+data.abc();
+console.log(data.obj);
+
+
+//Note : You need to add type : module property into package.json file i order this to work
