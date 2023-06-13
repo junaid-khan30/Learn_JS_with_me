3db9dce3a280be50422d4a69cf36ec487c30fff9
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-13 20:45:42 +0530

    array-helper

--- CS_/array-helpers/3.js
+++ CS_/array-helpers/3.js
@@ -1,6 +1,11 @@
 var arr = [100, 99, 10, 1000, 88, 99, 99];
 var result = [];
+//Method 1 : Imperative
+// for (var i = 0; i < arr.length; i++) {
+//     result.push(arr[i] * 2);
+// }
 
-arr.forEach( (ele)=>{
-    result.push(arr[i]);
-})
+//Method 2  : forEach
+arr.forEach(ele => result.push(ele * 2));
+
+console.log(result);
