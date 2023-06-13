1a957b428822bd42de22e4c4b1a1107c1b9f0768
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-13 22:04:36 +0530

    scopes

--- CS_/scopes/5.js
+++ CS_/scopes/5.js
@@ -1,7 +1,8 @@
 let a = "fun";
-//let a = "sun";
+ a = "sun";
 
 const b = 'cake';
-b = "sweet";
+//b = "sweet";
 console.log(a, b);
 //NOTE : if you declare any variable with const redeclaring is not possible
+//redeclaring is possible with let 
