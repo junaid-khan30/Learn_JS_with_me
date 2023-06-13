89072fe55947ca241a02d4febfeb260be9ec8aeb
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-13 20:48:34 +0530

    array-helper

---
+++ CS_/array-helpers/7.js
@@ -0,0 +1,16 @@
+var abc = function () {
+    console.log("I am function abc");
+}
+
+var obj = {
+    a: 1,
+    b: 11
+}
+
+export default {
+    abc, obj
+}
+
+// module.exports = {
+//     abc, obj
+// }
