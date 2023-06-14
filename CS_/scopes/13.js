7e0a5407fd15fc92c446128f5c6db7e894fc996b
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-14 20:57:37 +0530

    scopes

--- CS_/scopes/13.js
+++ CS_/scopes/13.js
@@ -1,10 +1,17 @@
 const obj = {
     fname: "Johnny",
     age: 55,
-    sayHai: () => {
-        console.log(obj.fname, obj.age);
+    sayHai: function ()  {
+        console.log(this.fname, this.age);
     }
 }
 console.log(obj.fname);
 console.log(obj.age);
 obj.sayHai();
+
+/*
+The keyword in Javascript refers to the current execution context 
+or the object on which a method is being invoked.
+
+It allows you to access properties and methods within the current object context
+*/
