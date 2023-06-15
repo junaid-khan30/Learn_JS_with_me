275d5c0624334ac57f67f7b3abcf1aa4d2d41210
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-15 13:19:20 +0530

    scopes

--- CS_/scopes/24.js
+++ CS_/scopes/24.js
@@ -3,9 +3,11 @@ var a = 10;
     {
         console.log(a);
         var a = 11;
+        
     }
    
     console.log(a);
     var a = 55;
+    
 }
 console.log(a);
--- CS_/scopes/25.js
+++ CS_/scopes/25.js
@@ -1,12 +1,11 @@
 let a = 10;
 {
+    let a = 99;
+    console.log(a);
     {
         let a = 11;
         console.log(a);
-        
     }
-    let a = 55;
-    console.log(a);
-    
 }
 console.log(a);
+//hoisting
