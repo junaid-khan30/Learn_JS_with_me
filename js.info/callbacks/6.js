cb6211d73019f6331e0bc7f050515e0ca2c12d3d
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-15 08:56:13 +0530

    highest rated phones

--- js.info/callbacks/6.js
+++ js.info/callbacks/6.js
@@ -2,6 +2,7 @@ var axios = require("axios");
 axios.get("https://dummyjson.com/products/category/smartphones")
 .then((res)=>{
     var products = res.data.products;
+    products.sort((a,b)=>b.rating - a.rating);
     console.log(products);
     
    
