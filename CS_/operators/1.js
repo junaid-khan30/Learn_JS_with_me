4471e77cbeb0a225076ab961d870334c729ff626
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-08-09 10:00:30 +0000

    operators

--- CS_/operators/1.js
+++ CS_/operators/1.js
@@ -2,10 +2,17 @@
 
 var a=2+5;
 a=2%5;
-var x=5, y=3;
+var x=2, y=-3;
 var result=1;
-for(var i=0; i<=y; i++){
+for(var i=1; i<=y; i++){
     result *=x;
 }
-console.log(result);
+console.log(`Using Loops : ${result}`);
+console.log(`Using ** : `,x**y);
+
+//Find Square root of a Number in Algorithm
+var n=25;
+
+console.log(`Square root of n is : `,n**0.5);
+
 
--- CS_/operators/2.js
+++ CS_/operators/2.js
@@ -1,13 +1,5 @@
-//Find Square roots using ** operators
-console.log(625**0.5);
-console.log(2**0.5);
-
-//In JS, We have squareroot function for this : 
-function squareroot(num){
-    return num ** 0.5;
-} 
-console.log(squareroot(100));
-
-var result = squareroot(125);
-console.log(result);
-
+//Write function to find Square of any Number 
+function squareRoot(num){
+    return num**0.5;
+}
+console.log(squareRoot(625)); 
