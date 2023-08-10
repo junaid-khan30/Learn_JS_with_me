2fb56eb204e2dbd86dbf9e41e7b7657d26bf4008
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-08-10 16:05:59 +0000

    readline-sync

--- CS_/cli-app/index.js
+++ CS_/cli-app/index.js
@@ -1,2 +1,6 @@
 var readlineSync = require('readline-sync');
 
+var fname = readlineSync.question("Wassup Buddy!!!!");
+var quote = readlineSync.question("What is fav quote??");
+
+console.log(fname,quote);
