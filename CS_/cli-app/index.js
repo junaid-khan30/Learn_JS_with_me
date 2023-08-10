e6591f994c19f2a08914dad844421b7e4d775999
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-08-10 16:11:17 +0000

    readline-sync

--- CS_/cli-app/index.js
+++ CS_/cli-app/index.js
@@ -6,5 +6,9 @@ var quote = readlineSync.question("What is fav quote??");
 //For integer Input
 var age = readlineSync.questionInt("Enter your age : ");
 
+//For email
+var email = readlineSync.questionEMail("Enter your email to get scammed  ");
+
 console.log(fname,quote);
 console.log(`Your age is ${age}`);
+console.log(`We keep spamming you at ${email}`);
