abd962570b96535aa2cb0892280f0b7af9452948
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-08-10 16:08:09 +0000

    readline-sync

--- CS_/cli-app/index.js
+++ CS_/cli-app/index.js
@@ -3,4 +3,8 @@ var readlineSync = require('readline-sync');
 var fname = readlineSync.question("Wassup Buddy!!!!");
 var quote = readlineSync.question("What is fav quote??");
 
+//For integer Input
+var age = readlineSync.questionInt("Enter your age : ");
+
 console.log(fname,quote);
+console.log(`Your age is ${age}`);
