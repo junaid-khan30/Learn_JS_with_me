6810e8c12d10214180013b6ddc5d2b711538b8f5
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-08-18 15:01:09 +0000

    alpha-conversion

---
+++ CS_/FS-PROB/alpha-conversion/1.js
@@ -0,0 +1,20 @@
+var fs = require("fs");
+fs.readFile('lowercase.txt', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    var fileData = data.toString().toUpperCase();
+    console.log(`File converted to Uppercase : ${fileData}`);
+   // fs.writeFile('uppercase.txt', fileData, (err) => {
+       //if (err) {
+        //    throw err;
+      //  }
+      //  console.log('Data written successfully!');
+       // fs.unlink('lowercase.txt', (err) => {
+          //  if (err) {
+           //     throw err;
+          //  }
+            console.log('File successfully deleted.');
+     //   });
+    });
+});
