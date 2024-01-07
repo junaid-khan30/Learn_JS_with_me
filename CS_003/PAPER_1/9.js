751e34988659975966788db195cd4241b4cc0334
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-07 14:30:20 +0000

    JS

---
+++ CS_003/PAPER_1/9.js
@@ -0,0 +1,23 @@
+var a = 15;
+var b = 10;
+console.log(a ^ b);
+console.log(a % b);
+console.log(20 ** 2);
+console.log(-15 << 2);
+console.log(15 << 2);
+console.log(10 >> 2);
+console.log(-15 >> 2);
+console.log(-22 >> 3);
+console.log(-22 >>> 3);
+
+/*
+console.log(a ^ b);: Outputs the result of the bitwise XOR operation between a and b (15 ^ 10). The result is 5.
+console.log(a % b);: Outputs the remainder of the division of a by b (15 % 10). The result is 5.
+console.log(20 ** 2);: Outputs the result of 20 raised to the power of 2 (20 squared). The result is 400.
+console.log(-15 << 2);: Outputs the result of left-shifting the binary representation of -15 by 2 positions. The result is -60.
+console.log(15 << 2);: Outputs the result of left-shifting the binary representation of 15 by 2 positions. The result is 60.
+console.log(10 >> 2);: Outputs the result of right-shifting the binary representation of 10 by 2 positions. The result is 2.
+console.log(-15 >> 2);: Outputs the result of right-shifting the binary representation of -15 by 2 positions. The result is -4.
+console.log(-22 >> 3);: Outputs the result of right-shifting the binary representation of -22 by 3 positions. The result is -3.
+console.log(-22 >>> 3);: Outputs the result of zero-fill right-shifting the binary representation of -22 by 3 positions. The result is 536870909.
+*/
