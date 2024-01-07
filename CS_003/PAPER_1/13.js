48b65a8b1fac20fc64a68986991679f436e48e4d
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-07 16:15:05 +0000

    JS

---
+++ CS_003/PAPER_1/13.js
@@ -0,0 +1,17 @@
+var fname = "prash";
+console.log(typeof fname);
+var value = true;
+console.log(typeof value);
+value = String(value);
+console.log(typeof value);
+
+/*
+var fname = "prash";: Declares a variable fname and assigns it the string value "prash".
+
+console.log(typeof fname);: Outputs the data type of fname, which is string.
+var value = true;: Declares a variable value and assigns it the boolean value true.
+
+console.log(typeof value);: Outputs the data type of value, which is boolean.
+value = String(value);: Converts the boolean value in value to its string representation.
+
+console.log(typeof value);: Outputs the data type of value after the conversion, which is now string. */
