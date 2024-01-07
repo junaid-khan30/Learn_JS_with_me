963b16d42b9ab79519bf0d58982ef3e45414d90a
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-07 16:10:22 +0000

    JS

--- CS_003/PAPER_1/10.js
+++ CS_003/PAPER_1/10.js
@@ -3,3 +3,12 @@ var j = 11.45;
 var p = -111.6567;
 var q = 22 / 7;
 console.log(typeof t, typeof j, typeof p, typeof q);
+/*var t = 11;: Declares a variable t and assigns it the integer value 11.
+
+var j = 11.45;: Declares a variable j and assigns it the floating-point value 11.45.
+
+var p = -111.6567;: Declares a variable p and assigns it the negative floating-point value -111.6567.
+
+var q = 22 / 7;: Declares a variable q and assigns it the result of the division operation 22 / 7, which is a floating-point approximation of π (pi).
+
+console.log(typeof t, typeof j, typeof p, typeof q);: Outputs the data types of t, j, p, and q, which are number, number, number, and number respectively. */
---
+++ CS_003/PAPER_1/11.js
@@ -0,0 +1,25 @@
+var str = "code.in";
+var fun = "booze";
+console.log(typeof str);
+console.log(str.length);
+console.log(str[0]);
+str = 'code..in' + fun + "do boo poo";
+console.log(`${str}its rainy today${fun}do boo poo`);
+console.log(str + "its rainy today" + fun + "do boo poo");
+
+/*
+var str = "code.in";: Declares a variable str and assigns it the string value "code.in".
+
+var fun = "booze";: Declares a variable fun and assigns it the string value "booze".
+
+console.log(typeof str);: Outputs the data type of str, which is string.
+
+console.log(str.length);: Outputs the length of the string in str, which is 7.
+
+console.log(str[0]);: Outputs the first character of the string in str, which is "c".
+
+str = 'code..in' + fun + "do boo poo";: Concatenates strings and updates the value of str.
+
+console.log(${str}its rainy today${fun}do boo poo);: Outputs a string that includes the value of str, followed by "its rainy today", the value of fun, and "do boo poo".
+
+console.log(str + "its rainy today" + fun + "do boo poo");: Outputs a string formed by concatenating the value of str with "its rainy today", the value of fun, and "do boo poo". */
