8176b09501cda867e262b55fb3dd85142967b602
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-07 13:45:30 +0000

    JS

---
+++ CS_003/PAPER_1/5.js
@@ -0,0 +1,13 @@
+var i = 0;
+while (i <= 10) {
+    console.log(i);
+    --i;
+}
+
+/*The given code block initiates a while loop with the condition i <= 10. 
+However, the loop is set up to continually decrement i (--i), 
+creating an infinite loop that never meets the exit condition. 
+As a result, the loop will continuously print the value of i, 
+which starts at 0 and decreases indefinitely. 
+The code will not terminate, and there won't be any output due to the infinite loop.
+ */
