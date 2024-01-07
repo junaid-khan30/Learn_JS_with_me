1070b7d6d80bdd2025eff7a9ab981c37de994005
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-07 16:23:48 +0000

    JS

---
+++ CS_003/PAPER_1/16.js
@@ -0,0 +1,12 @@
+var anina = "22";
+var walter = "11";
+console.log(anina + walter);
+console.log(+anina + +walter);
+
+/*
+In the code, two variables anina and walter are initialized with string values "22" and "11" respectively.
+The first console.log(anina + walter) concatenates the two strings, resulting in the string "2211" being printed.
+The second line uses the unary + operator to convert the string values to numbers and then adds them. 
+ +anina converts the string "22" to the number 22, and +walter converts "11" to the number 11
+ The sum of these two numbers is then printed using console.log(+anina + +walter),
+*/
