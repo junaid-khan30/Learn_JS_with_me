f664e4895409347712781bc3a2f6075304ea8583
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-07 16:19:03 +0000

    JS

---
+++ CS_003/PAPER_1/14.js
@@ -0,0 +1,14 @@
+
+var a = "6";
+var b = "9";
+console.log(typeof a);
+a = Number(a);
+console.log(typeof a);
+
+/*
+The code initializes two variables, a and b, with the string values "6" and "9" respectively. 
+The console.log(typeof a) statement outputs the data type of variable a, which is initially a string, so it prints "string". 
+Then, the code converts the value of a to a number using Number(a), and the result is assigned back to a. 
+The subsequent console.log(typeof a) statement outputs the new data type of a after the conversion, which is now a number. 
+
+*/
