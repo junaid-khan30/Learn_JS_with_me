1698f9fabb913761d9e5aa1ff19431d7456f0de8
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-08 19:01:49 +0000

    JS

---
+++ CS_003/PAPER_1/22.js
@@ -0,0 +1,24 @@
+var a = 4;
+var b = 0;
+console.log(a + b);
+var string = +'456';
+console.log(typeof a);
+console.log(typeof string);
+var str1 = "The";
+var str2 = "School";
+console.log(str1 + str2);
+
+/*
+console.log(a + b); - Outputs the result of adding a and b, which is 4. The + operator is used for addition in this context.
+
+var string = +'456'; - Converts the string '456' to a number using the unary + operator. The result is stored in the variable string.
+
+console.log(typeof a); - Outputs the data type of variable a, which is a number.
+
+console.log(typeof string); - Outputs the data type of the variable string, which is also a number after the conversion.
+
+console.log(str1 + str2); - Concatenates the strings stored in variables str1 and str2, resulting in the output 'TheSchool'.
+
+In summary, the code demonstrates addition of numeric variables, conversion of a string to a number using the unary + operator, and concatenation of two strings. 
+The output types are determined using the typeof operator.
+*/
