7b79213dcba837da059eac92204c5d5a16aff0ca
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-08 18:51:08 +0000

    JS

---
+++ CS_003/PAPER_1/19.js
@@ -0,0 +1,22 @@
+console.log(9 % 5);
+console.log(9 ** 2);
+console.log(16 ** 0.5);
+console.log(2 ** 0.5);
+console.log(625 ** 0.5);
+console.log(15 << 2);
+
+/*
+console.log(9 % 5); - Outputs the remainder of the division of 9 by 5, which is 4. The % operator represents the modulo operation.
+
+console.log(9 ** 2); - Outputs the result of 9 raised to the power of 2, which is 81. The ** operator represents the exponentiation operation.
+
+console.log(16 ** 0.5); - Outputs the square root of 16, which is 4. The ** operator with a fractional exponent represents the root operation.
+
+console.log(2 ** 0.5); - Outputs the square root of 2, which is approximately 1.4142135623730951.
+
+console.log(625 ** 0.5); - Outputs the square root of 625, which is 25.
+
+console.log(15 << 2); - Outputs the result of left-shifting the binary representation of 15 by 2 positions, 
+which is equivalent to multiplying 15 by 2^2 (4), 
+resulting in 60. The << operator represents the left shift operation.
+*/
