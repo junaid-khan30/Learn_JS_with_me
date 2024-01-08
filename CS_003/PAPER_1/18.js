782379c4ea965d2871769f4f7c04f073e104c8e7
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-08 18:44:47 +0000

    JS

---
+++ CS_003/PAPER_1/18.js
@@ -0,0 +1,25 @@
+var a = 22;
+var b = 7;
+console.log(a / b);
+console.log(Math.floor(a / b));
+console.log(Math.ceil(a / b));
+console.log(Math.round(7 / 3));
+console.log(7 / 3);
+console.log(Math.round(1.41234));
+console.log(Math.round(1.61234));
+
+/*
+console.log(a / b); - Outputs the result of the division of a by b, which is 22 / 7 ≈ 3.142857142857143.
+
+console.log(Math.floor(a / b)); - Outputs the floor value of the division, the largest integer less than or equal to the result. In this case, it's 3.
+
+console.log(Math.ceil(a / b)); - Outputs the ceiling value of the division, the smallest integer greater than or equal to the result. In this case, it's 4.
+
+console.log(Math.round(7 / 3)); - Outputs the rounded value of 7 / 3, which is 2. The Math.round() function rounds to the nearest integer.
+
+console.log(7 / 3); - Outputs the direct result of the division, which is 7 / 3 ≈ 2.3333333333333335.
+
+console.log(Math.round(1.41234)); - Outputs the rounded value of 1.41234, which is 1.
+
+console.log(Math.round(1.61234)); - Outputs the rounded value of 1.61234, which is 2.
+*/
