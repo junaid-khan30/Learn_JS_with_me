41d182e827a2e03239ec2157279d3a222b77e919
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-08 19:49:39 +0000

    JS

---
+++ CS_003/PAPER_1/28.js
@@ -0,0 +1,18 @@
+var t = 0;
+if (0) {
+    console.log("in");
+}
+else {
+    console.log("out");
+}
+
+
+/*
+The if statement checks the truthiness of the expression inside the parentheses. In this case, the expression is 0.
+
+JavaScript treats 0 as falsy in a boolean context. Therefore, the condition in the if statement evaluates to false.
+
+Since the condition is false, the code inside the else block is executed.
+
+The console.log("out"); statement inside the else block is executed, and "out" is printed to the console
+*/
