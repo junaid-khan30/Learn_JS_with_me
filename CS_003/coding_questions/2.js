8d6b1c8083f3f281576b0a0ba3c559f5030054ac
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-13 18:52:43 +0000

    JS

---
+++ CS_003/coding_questions/2.js
@@ -0,0 +1,8 @@
+/*
+Write a JavaScript program to generate all the prime numbers between given input ranges and print them in descending order.
+
+Input : 
+The first line should contain the M and N input ranges.
+Output : 
+The output should be the prime numbers between the M and N in descending order. 
+*/
