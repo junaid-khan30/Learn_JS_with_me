2bd7b0f489125d04d92f6837760256e9a92efc55
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-13 19:03:52 +0000

    JS

---
+++ CS_003/coding_questions/9.js
@@ -0,0 +1,13 @@
+/*Build a CLI project in JavaScript that calculates the MGPA.
+
+Inputs for MGPA:
+Student Name
+MAT Number
+Number of Courses
+Course Names
+Course Credits
+Obtained Grade Points
+
+Output for MGPA:
+All input details along with calculated MGPA.
+Report card details should be displayed in table format */
