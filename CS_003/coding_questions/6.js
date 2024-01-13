3fb21cc94e255b6a046835bfcdbd20a8c1a8614a
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-13 19:02:11 +0000

    JS

---
+++ CS_003/coding_questions/6.js
@@ -0,0 +1,31 @@
+/*
+.  Write a JavaScript program to convert the decimal to octal and extract even and odd numbers from the octal number.
+Input : 
+The first line should be the decimal number N.
+Output : 
+The first line of the output should be the converted octal number. 
+The second line of the output should be two integers, the first one is the number of even numbers and the second one is the number of odd numbers which are in the octal number.
+
+Constraints : 
+
+N = Input Decimal number
+1 <= N <= 1000
+Output even and odd integers should be separated with a space (" ")
+
+
+Sample Input 1 :
+546
+
+Sample Output 1 :
+1042
+3 1
+
+
+
+Sample Input 2 :
+54
+
+Sample Output 2 :
+66
+2 0
+*/
