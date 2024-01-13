4dfbb9797bf136d787847894f9e7df0b58bb9fe5
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-13 19:01:07 +0000

    JS

---
+++ CS_003/coding_questions/5.js
@@ -0,0 +1,27 @@
+/*
+Write a JavaScript program to find the Factorial of a number and print that number in reverse order.
+Input : 
+The first line should be the integer N.
+Output :
+The output should be the factorial of N in reverse order. 
+
+Constraints : 
+
+N = Number to find the factorial
+1 <= N <= 20
+
+
+Sample Input 1 :
+3
+
+Sample Output 1 :
+6
+
+
+
+Sample Input 2 :
+5
+
+Sample Output 2 :
+021
+*/
