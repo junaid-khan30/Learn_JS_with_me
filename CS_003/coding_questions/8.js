a44a405ff2c0865b666492c53429c378128a3f97
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-13 19:03:20 +0000

    JS

---
+++ CS_003/coding_questions/8.js
@@ -0,0 +1,34 @@
+/*
+Create a JavaScript program that prints the following pattern. 
+Input : 
+The first line should contain the M rows.
+Output : 
+The output should be the pattern in the requested format. 
+
+Constraints : 
+
+M = Number or rows to be printed.
+1 <= M <= 15
+
+
+Sample Input 1 :
+4
+Sample Output 1 :
+    0 x 9 + 8 = 8
+    9 x 9 + 7 = 88
+  98 x 9 + 6 = 888
+987 x 9 + 5 = 8888
+
+
+
+Sample Input 2 :
+6
+
+Sample Output 2 :
+        0 x 9 + 8 = 8
+        9 x 9 + 7 = 88
+      98 x 9 + 6 = 888
+    987 x 9 + 5 = 8888
+  9876 x 9 + 4 = 88888
+98765 x 9 + 3 = 888888
+*/
