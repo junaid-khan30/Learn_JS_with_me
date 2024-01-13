5e5767fc4ba609e3d3be8cc7cf6189d241e99f9f
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-13 19:00:34 +0000

    JS

---
+++ CS_003/coding_questions/4.js
@@ -0,0 +1,24 @@
+/*Write a JavaScript program to read 10 integers, store them in an array, and sort the array in ascending order even it contains duplicate elements.
+Input : 
+The first line should be the  10 integers.
+Output :
+The output should be the sorted array. 
+
+Constraints : 
+
+Output array elements should be enclosed in square brackets ("[]"), separated by commas (",") followed by a space (" ").
+
+
+Sample Input 1 :
+8 4 4 3 6 3 2 1 9 0
+
+Sample Output 1 :
+[0, 1, 2, 3, 3, 4, 4, 6, 8, 9]
+
+
+
+Sample Input 2 :
+4 7 3 7 5 2 9 3 0 0
+
+Sample Output 2 :
+[0, 0, 2, 3, 3, 4, 5, 7, 7, 9] */
