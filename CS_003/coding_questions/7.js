943f61e35c0d0361441992bed4d38a031e386fe1
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-13 19:02:44 +0000

    JS

---
+++ CS_003/coding_questions/7.js
@@ -0,0 +1,32 @@
+/*
+Write a JavaScript Program to convert the given number of days in terms of years, weeks & days.
+
+Input : 
+The first line should be the number of days N.
+Output :
+The output should display the numbers of years, months, and days from the input days. 
+
+Constraints : 
+
+N = Number of days
+1 <= N <= 10000
+
+
+Sample Input 1 :
+768
+
+Sample Output 1 :
+2 Years
+1 Month
+8 Days
+
+
+
+Sample Input 2 :
+835
+
+Sample Output 2 :
+2 Years
+3 Months
+15 Days
+*/
