c5b16db01dcfb0620dc219e0785dba18ea33e5cf
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-14 13:57:44 +0000

    JS Intro

--- CS_/Introduction/4.js
+++ CS_/Introduction/4.js
@@ -10,4 +10,9 @@ var arr = [1,5,6,4,9];
 var salary = 90000.95;
 
 console.log(arr);
-console.log(35 + 45.80);
+console.log((35 + 45.80).toFixed(52));
+console.log((0.1+0.2).toFixed(52));
+
+//Loosely typed Language
+//We don't have to worry about storage and all , it happens internally
+
---
+++ CS_/Introduction/5.js
@@ -0,0 +1,4 @@
+var story = "Monkey \" eat in class";
+var story2 = 'I \'love \' making\' money';
+console.log(story);
+console.log(story2);
