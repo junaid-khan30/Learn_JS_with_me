6210c298670fa67ad5d604ab22364b7d9dd1c336
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-14 15:33:30 +0000

    JS for loop

--- CS_/Introduction/loops/5.js
+++ CS_/Introduction/loops/5.js
@@ -2,3 +2,13 @@
 var str = "HELLO";
 var result = str.split("").reverse().join("");
 console.log(result);
+
+var char = "Hello Good Evening Freinds";
+
+console.log(char.split());
+console.log(char.split(''));
+console.log(char.split(' '));
+//Word Count
+console.log(char.split(' ').length);
+//Character Counter
+console.log(char.split('').length);
