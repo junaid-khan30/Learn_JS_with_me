105f6f1a4f85a63252faa757a9a9909b993dddd2
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-14 16:45:36 +0000

    JS for loop

--- CS_/Introduction/loops/6.js
+++ CS_/Introduction/loops/6.js
@@ -1,7 +1,14 @@
 var str = "Hyderabad";
 
-console.log(str[5]);
-console.log(str.at(5));
+// console.log(str[5]);
+// console.log(str.at(5));
 
-console.log(str[-1]);
-console.log(str.at(-1));
+// console.log(str[-1]);
+// console.log(str.at(-1));
+
+var temp = "";
+for(var i=str.length -1; i>=0; i--){
+    
+    console.log(`${temp}${str[i]} `);
+    temp += str[i];
+}
