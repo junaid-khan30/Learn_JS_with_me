08dea6bd1b307cb299e9696f24129e8fb5342124
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-01-14 14:20:10 +0000

    JS for loop

---
+++ CS_/Introduction/loops/1.js
@@ -0,0 +1,10 @@
+var str = "The CS.CODE.IN";
+
+console.log(str);
+console.log(str.length);
+console.log(str[0]);
+console.log(str[1]);
+
+for(var i=0; i<str.length; i++){
+    console.log(str[i]);
+}
