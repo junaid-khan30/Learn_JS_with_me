1042bc4833928121740d7e46cefb110487c2fde0
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-02-07 00:53:36 +0530

    loops

--- CS_/Introduction/loops/7.js
+++ CS_/Introduction/loops/7.js
@@ -1,3 +1,15 @@
 // Question 1: Calculate the factorial of a given number using a while loop.
 
+function calculateFactorial(num) {
+    let factorial = 1;
+    let i = 1;
   
+    while (i <= num) {
+      factorial *= i;
+      i++;
+    }
+  
+    return factorial;
+  }
+  
+  console.log("Factorial of 5:", calculateFactorial(5));
