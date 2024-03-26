98748b029bd74d77202e72aa3de711989c1990fb
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-03-26 18:07:52 +0530

    array-helpers

---
+++ CS_/array-helpers/gitFiles/2.js
@@ -0,0 +1,8 @@
+// Map
+const doubleArray = array => array.map(item => item * 2);
+
+// Filter
+const evenNumbers = array => array.filter(item => item % 2 === 0);
+
+// Reduce
+const sumArray = array => array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
