48f56fb589587448f955ec6bf4045af8f418e343
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-03-28 22:08:07 +0530

    array-helpers

--- CS_/array-helpers/gitFiles/3.js
+++ CS_/array-helpers/gitFiles/3.js
@@ -8,3 +8,7 @@ for (let i = 0; i < numbers.length; i++) {
   doubledNumbers.push(numbers[i] * 2);
 }
 console.log(doubleDNumbers); // Output: [2, 4, 6, 8, 10]
+
+
+const numBers = [1, 2, 3, 4, 5];
+
