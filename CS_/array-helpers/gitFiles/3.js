976b0b059971ce9186e5db1fa7e52f55072f9115
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-03-28 22:09:57 +0530

    array-helpers

--- CS_/array-helpers/gitFiles/3.js
+++ CS_/array-helpers/gitFiles/3.js
@@ -28,4 +28,6 @@ for (let i = 0; i < numbers.length; i++) {
   const numbers = [1, 2, 3, 4, 5];
   const sum = numbers.reduce((acc, num) => acc + num, 0);
   console.log(sum); // Output: 15
-  
+  const numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+
