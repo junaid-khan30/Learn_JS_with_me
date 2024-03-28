bff40dff6081e069f54847e07bb0e3ad87c1e85a
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-03-28 22:07:39 +0530

    array-helpers

--- CS_/array-helpers/gitFiles/3.js
+++ CS_/array-helpers/gitFiles/3.js
@@ -1,3 +1,10 @@
 const numbers = [1, 2, 3, 4, 5];
 const doubledNumbers = numbers.map(num => num * 2);
 console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
+
+const nuMbers = [1, 2, 3, 4, 5];
+const doubleDNumbers = [];
+for (let i = 0; i < numbers.length; i++) {
+  doubledNumbers.push(numbers[i] * 2);
+}
+console.log(doubleDNumbers); // Output: [2, 4, 6, 8, 10]
