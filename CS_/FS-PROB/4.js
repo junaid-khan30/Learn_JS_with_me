4ef0965c0801b8cebbd9bf0d4424eead0a4d7ba2
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-04-03 23:23:10 +0530

    fs redline

--- CS_/FS-PROB/4.js
+++ CS_/FS-PROB/4.js
@@ -4,5 +4,9 @@ const newContent = 'This is the new content to append to the file.';
 
 // Appending to a file asynchronously
 fs.appendFile('example.txt', newContent, (err) => {
-  
+    if (err) {
+        console.error('Error appending to file:', err);
+        return;
+      }
+      
 });
