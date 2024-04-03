76c603698862f196b9bf539484668ce963c3cbfc
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-04-03 23:25:12 +0530

    fs redline

--- CS_/FS-PROB/5.js
+++ CS_/FS-PROB/5.js
@@ -15,4 +15,9 @@ const fs = require('fs');
 
 // Checking if a file exists
 fs.access('example.txt', fs.constants.F_OK, (err) => {
-  
+    if (err) {
+        console.error('File does not exist or is not accessible.');
+        return;
+      }
+      console.log('File exists and is accessible.');
+    });
