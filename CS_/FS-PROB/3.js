aeae5cbcaf4437a767a7be73cd7ef83d291b22f0
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-04-03 23:20:36 +0530

    fs redline

--- CS_/FS-PROB/3.js
+++ CS_/FS-PROB/3.js
@@ -2,5 +2,9 @@ const fs = require('fs');
 
 // Reading from a file asynchronously
 fs.readFile('example.txt', 'utf8', (err, data) => {
-  
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+      }
+      
 });
