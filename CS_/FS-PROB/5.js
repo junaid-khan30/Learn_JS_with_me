047f47668d2b598765be5f8de79736b0d4ac756c
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-04-03 23:23:55 +0530

    fs redline

---
+++ CS_/FS-PROB/5.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const content = 'This is the content to write to the file.';
+
+// Writing to a file asynchronously
+fs.writeFile('example.txt', content, (err) => {
+  
+});
