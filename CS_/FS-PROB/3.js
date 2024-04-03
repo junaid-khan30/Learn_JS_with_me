e1538367d500491fac826c5949641b92326e2a13
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-04-03 23:22:22 +0530

    fs redline

--- CS_/FS-PROB/3.js
+++ CS_/FS-PROB/3.js
@@ -8,3 +8,11 @@ fs.readFile('example.txt', 'utf8', (err, data) => {
       }
       console.log('File content:', data);
 });
+
+/*
+This code snippet imports the Node.js file system module (fs). 
+It then asynchronously reads the content of a file named "example.txt" in UTF-8 encoding. 
+If an error occurs during the reading process, it logs the error message to the console. 
+Otherwise, it prints the content of the file to the console. The callback function receives two parameters: err,
+ which contains any error that may occur during the reading process, and data, which contains the content of the file once it's read.
+*/
