f3c1d47992030085a21879a0e464ff534473230a
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-04-22 23:08:28 +0530

    loops

--- loops/10.js
+++ loops/10.js
@@ -18,3 +18,7 @@ for (let i = 1; i <= num; i++) {
     factorial *= i;
 }
 console.log("Factorial of", num, "is", factorial);
+
+let num = parseInt(prompt("Enter a number: "));
+let isPrime = true;
+
