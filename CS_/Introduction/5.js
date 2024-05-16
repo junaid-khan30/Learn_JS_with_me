ba4279c88d7f586100db9790a656d8b579b755b2
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-05-16 22:44:45 +0530

    asdf

--- CS_/Introduction/5.js
+++ CS_/Introduction/5.js
@@ -2,3 +2,8 @@ var story = "Monkey \" eat in class";
 var story2 = 'I \'love \' making\' money';
 console.log(story);
 console.log(story2);
+
+
+// Question: What does the following code do?
+function multiply(a, b) {
+    
