d9c6a21b7e72c56c03be55c893fedff0619235d7
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-08-14 14:53:29 +0530

    JS

---
+++ CS_/Introduction/hello.js
@@ -0,0 +1,16 @@
+//Data types 
+//you don't have to strictly put data types whether it be float, string, Num, arrays
+//for eg you can just put var, let, const to assign any data types
+var num = 45;
+
+var fname = "Prash";
+
+var lname = "B";
+
+var arr = [1,2,5,9,20];
+
+var salary = 4500.45;
+console.log(arr);
+console.log(0.1 + 0.2);
+
+//but it internally it recognizes its datatypes, but to us it won't show, 
